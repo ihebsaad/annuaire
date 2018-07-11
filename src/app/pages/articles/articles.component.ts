1e6b96a0ae67b5678b36c17afce885fd77f50f9c
iheb <ihebsaad@gmail.com>
2018-07-11 09:45:02 +0100

    fix error

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AnnoncesService} from './articles.service';
+import {ArticlesService} from './articles.service';
 import {LocalDataSource} from 'ng2-smart-table';
 
 
