10f5f5df0b25bb7a1cdc338d0f3a418c0aee8e4e
oussama9411 <oussama.hermessi.professional@gmail.com>
2018-07-20 16:06:39 +0100

    update model repertoire.js

--- server/models/repertoire.js
+++ server/models/repertoire.js
@@ -1,34 +1,33 @@
 var mongoose = require('mongoose');
- 
-  
+
+
 var repertoireSchema = mongoose.Schema({
-	titre: {
-		type: String,
-		required:true
-	},
-	categorie: {
-		type: String,
-		required:true
-	},
-	tel: {
-		type: Number,
-		required:false
-	},
-	ville: {
-		type: String,
-		required:true
-	},
-	adresse: {
-		type: String,
-		required:true
-	},	
-	auteur: {
-		type: String,
-		required:true
-	} 
+    titre: {
+        type: String,
+        required:true
+    },
+    categorie: {
+        type: String,
+        required:true
+    },
+    tel: {
+        type: String,
+        required:false
+    },
+    ville: {
+        type: String,
+        required:true
+    },
+    adresse: {
+        type: String,
+        required:true
+    },
+    auteur: {
+        type: String,
+        required:true
+    }
 });
 
- 
- 
+
+
 let Repertoire = module.exports = mongoose.model('Repertoire', repertoireSchema);
- 
