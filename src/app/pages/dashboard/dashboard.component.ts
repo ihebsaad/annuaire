d86479ce70a7a2d3f918abd982acf467408d2303
raniasaad <saadrania@92gmail.com>
2018-08-09 11:08:05 +0100

    dynamic categories

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,18 +3,26 @@ import {Location} from './search-map/entity/Location';
 import {Router} from '@angular/router';
 import {MapComponent} from './search-map/map/map.component';
 import {DataService} from './data.service';
-
+import {CategoriesService} from '../categories/categories.service';
 
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './dashboard.component.html', styleUrls: ['./dashboard.component.scss'],
+  providers: [CategoriesService]
 })
 export class DashboardComponent implements OnInit {
     searchedLocation: Location = new Location();
     source = 'assets/tunisie.jpg';
     radioModel = 'left';
-    constructor(private data: DataService, private router: Router) {
+    dataC:any;
+    constructor(private data: DataService, private router: Router, private servCateg:CategoriesService) {
+     this.servCateg.getData().subscribe(resp => {console.log(resp);
+          console.log(resp['categories']);
+          this.dataC = resp['categories'];
+console.log(this.dataC);
+      });
+
     }
 
     ngOnInit() {
@@ -26,4 +34,15 @@ export class DashboardComponent implements OnInit {
         this.data.changeLocation(this.searchedLocation);
         this.router.navigate(['pages/map']);
     }
+
+      getData() {
+
+      this.servCateg.getData().subscribe(resp => {console.log(resp);
+          console.log(resp['categories']);
+          this.dataC = resp['categories'];
+console.log(this.dataC);
+      });
+
+  }
+
 }
