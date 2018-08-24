efd0cd7af483e0fdd053793a3dd6ff8c2f8a9054
iheb <ihebsaad@gmail.com>
2018-08-24 10:24:04 +0100

    another try

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -22,7 +22,7 @@ import {ModalComponent4} from './articles/modal/modal.component';
 import {ModalComponent5} from './users/modal/modal.component';
 import {FormsModule} from '@angular/forms';
 import {Ng2PaginationModule} from 'ng2-pagination';
-import { FileSelectDirective } from 'ng2-file-upload';
+import { FileUploadModule } from 'ng2-file-upload';
 import { HttpModule } from '@angular/http';
 import { RateComponent } from './dashboard/rate/rate.component';
 const PAGES_COMPONENTS = [
@@ -38,7 +38,7 @@ const PAGES_COMPONENTS = [
     MiscellaneousModule,
     Ng2SmartTableModule,
       FormsModule,HttpModule,
-      Ng2PaginationModule,FileSelectDirective
+      Ng2PaginationModule,FileUploadModule
   ],
   declarations: [
     ...PAGES_COMPONENTS,
