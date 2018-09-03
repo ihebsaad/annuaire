b629b6ca7dd68194539f7c587cd48af7cf5195a7
iheb <ihebsaad@gmail.com>
2018-09-03 11:14:05 +0100

    test url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-URL="";
+//URL="";
 /*if (window.location.hostname == 'localhost'){
     URL= 'http://'+window.location.hostname+':3000';
 }else {
