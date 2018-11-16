8aa185ea8c8dd17cda1ce081e179bf3cc3b13a3e
iheb <ihebsaad@gmail.com>
2018-11-16 10:38:48 +0100

    tesst

--- server/app.js
+++ server/app.js
@@ -170,6 +170,8 @@ function onListening() {
    
 /*************  Pictures  ******************
 
+ /*************  Pictures  *****************/
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -177,9 +179,9 @@ const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 
- 
+
 // view engine setup
- app.set('uploads', path.join(__dirname, 'uploads'));
+app.set('uploads', path.join(__dirname, 'uploads'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -190,44 +192,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(fileUpload());
-////app.use('/uploads', express.static(__dirname + '/uploads'));
+//app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use('/uploads',express.static('uploads'));
 
 app.post('/upload', (req, res, next) => {
-  console.log(req);
-  let imageFile = req.files.file;
+    console.log(req);
+    let imageFile = req.files.file;
 
-  imageFile.mv(`${__dirname}/uploads/${req.body.filename}.jpg`, function(err) {
-    if (err) {
-      return res.status(500).send(err);
-    }
+    imageFile.mv(`${__dirname}/uploads/${req.body.filename}.jpg`, function(err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
 
-    res.json({file: `uploads/${req.body.filename}.jpg`});
-  });
+        res.json({file: `uploads/${req.body.filename}.jpg`});
+    });
 
 })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(8888, () => {
-  console.log('8888');
+    console.log('8888');
 });
 
- */
+
 module.exports = app;
