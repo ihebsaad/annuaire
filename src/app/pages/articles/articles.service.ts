cbcc213cdd334bc0d837a3b8dfc5c28a59ced463
iheb <ihebsaad@gmail.com>
2018-11-27 16:28:05 +0100

    fix annonces problem

--- src/app/pages/articles/articles.service.ts
+++ src/app/pages/articles/articles.service.ts
@@ -50,7 +50,6 @@ export class ArticlesService {
         'Access-Control-Allow-Origin': '*',
       }),
     };
-    alert(obj.contenu);
     return this.http.post(this.configUrl + '/articles/add', obj, httpOptions);
 
   }
